4573370897f086081c63c672d45a05d127933a4b
Gregory Banks <gbanks1438@gmail.com>
2021-10-21 20:35:36 -0400

    commit tree 33f2edb2c4f017a8a36c42578f7bfa6b52fa7119

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+const USER_URL = "http://localhost:3000/users"
+
+let bodyData = {
+    name: "Steve",
+    email: "steve@steve.com"
+}
+
+let objStuff = {
+    method: "POST",
+    headers: {
+        "Content-type": "application/json",
+        "accept": "application/json"
+    },
+    body: JSON.stringify(bodyData)
+}
+
+function submitData () {
+    return fetch(USER_URL, objStuff)
+    .then(response => response.json())
+    .then(getID)
+    .catch(errorFunc)
+}
+
+const bodyTag = document.querySelector("body")
+
+function getID(object) {
+    document.body.innerHTML = object.id
+}
+
+function errorFunc(error) {
+    document.body.innerHTML = error.message
+}
